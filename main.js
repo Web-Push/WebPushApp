7d0bdf8f2ce456554e2c20d4c602cbae259b9f17
Web-Push <cloudlearning777@gmail.com>
2016-03-14 20:01:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -213,7 +213,7 @@ window.addEventListener('load', function() {
   // Check that service workers are supported, if so, progressively
   // enhance and add push messaging support, otherwise continue without it.
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('https://web-push.github.io/k_y_test/service-worker.js').
+    navigator.serviceWorker.register('https://web-push.github.io/k_y_test/service-worker.js', {scope: './'}).
       then(function(registration) {
     　   // 登録成功
     　   console.log('ServiceWorker registration successful with scope: ', registration.scope);
