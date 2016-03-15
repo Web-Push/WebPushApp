4cdea3eae252abc8307197994b61e39fa55a3775
Web-Push <cloudlearning777@gmail.com>
2016-03-15 15:43:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -211,7 +211,23 @@ window.addEventListener('load', function() {
   });
   console.log('screen.width:'+ screen.width);
   console.log('screen.height:'+ screen.height);
-
+    var _ua = (function(u){
+      return {
+        Tablet:(u.indexOf("windows") != -1 && u.indexOf("touch") != -1 && u.indexOf("tablet pc") == -1) 
+          || u.indexOf("ipad") != -1
+          || (u.indexOf("android") != -1 && u.indexOf("mobile") == -1)
+          || (u.indexOf("firefox") != -1 && u.indexOf("tablet") != -1)
+          || u.indexOf("kindle") != -1
+          || u.indexOf("silk") != -1
+          || u.indexOf("playbook") != -1,
+        Mobile:(u.indexOf("windows") != -1 && u.indexOf("phone") != -1)
+          || u.indexOf("iphone") != -1
+          || u.indexOf("ipod") != -1
+          || (u.indexOf("android") != -1 && u.indexOf("mobile") != -1)
+          || (u.indexOf("firefox") != -1 && u.indexOf("mobile") != -1)
+          || u.indexOf("blackberry") != -1
+      }
+    })(window.navigator.userAgent.toLowerCase());
   // Check that service workers are supported, if so, progressively
   // enhance and add push messaging support, otherwise continue without it.
   if ('serviceWorker' in navigator) {
